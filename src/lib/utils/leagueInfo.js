c8e44b138262587eeec8b90a7114b007e7c7363c
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2023-01-15 06:45:28 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -2,7 +2,7 @@
 export const leagueID = "873246287271792640"; // your league ID
 export const leagueName = "The Legends League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
-export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
+export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
