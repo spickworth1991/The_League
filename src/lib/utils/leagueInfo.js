85e9ed3f1f519474ebec841b76611e6e87370778
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2023-01-15 08:28:17 -0500

    Revert "Update leagueInfo.js"
    This reverts commit fc568aae0558b13e9149e529c1bf72ac5fe7eab9.

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -3,7 +3,7 @@ export const leagueID = "920326259551375360"; // your league ID
 export const leagueName = "The League"; // your league name
 export const dues = 25; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
